0f476b2eac34934e85154e9bd56db566a7ef663f
Lisa-MarieRakotoson <l.rakotoson@yahoo.com>
2026-06-12 16:05:02 +0200

    Ajout de la page gestion batiment manquante
    le même commentaire que le commit Update gestion_batE

---
+++ PrototypeSAe23/gestion_batB.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['gestion'])) {
+    header("Location: gestion_batB.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Page Admin</title>
+    <link rel="stylesheet" type="text/css" href="./styles/style.css" />
+        <!-- Menu -->
+    <header>
+		 <nav>
+			<ul>		
+				<li><a href="index.html" >Accueil</a></li>
+				<li><a href="login.php" >Se connecter</a></li>
+				<li><a href="consult.html">Consultation</a></li>
+				<li><a href="gestion.html">Gestion du projet</a></li>
+		</ul> 
+	</nav>
+ </header>
+</head>
+<body>
+    <h1>Bienvenue, <?php echo htmlspecialchars($_SESSION['gestion']); ?> !</h1>
+    <p>Tu es sur la page réservée a la gestion de base de données concernant le bâtiment B.</p>
+
+    <a href="logout.php">Se déconnecter</a>
+</body>
+</html>
