007f212738426ff703c680bc1015b5adaaf59de5
Lisa-MarieRakotoson <l.rakotoson@yahoo.com>
2026-06-19 14:47:06 +0200

    Ajout mentions légales + update
    Ajout de la page php mentions légale et mis à jour du fichier footer qui inclut les étiquettes de vérification de code HTML et CSS et les mentions légales en cliquable

---
+++ SAe23_PHP/pages_php/MentionsLegales.php
@@ -0,0 +1,36 @@
+<?php
+/* gestion_projet.php
+ * Project management page – accessible to everyone.
+ */
+
+$page_title   = 'Gestion de projet – SAE23 IUT Blagnac';
+$current_page = 'projet';
+
+require_once 'header.php';
+?>
+  
+  <section id="first">
+	   <h2> Mentions légales </h2>
+	<p>
+	<strong>Editeurs :</strong> Rakotoson Lisa-Marie, Peyre Ethan, Tassin Mathéo et Dewatine Julien <br>
+	<strong>Email :</strong> lisa-marie.rakotoson@ikmail.com, ethan.peyre.iut@gmail.com, matheo.tassin@gmail.com, juliendewatine@gmail.com <br>
+	</p>
+  </section>
+  
+<section>
+	   <h2> Hébergement </h2>
+	<p>
+	<strong>Hébergeur:</strong> local (sserveur XAMPP)
+	</p>
+  </section>
+  
+  <section>
+	<p>
+	Conformément à la loi Informatique et Libertés, nous vous informons que nous ne collectons <strong>aucune donnée personnelle</strong>
+	</p>
+  </section>
+  <aside id="last">
+    
+<?php
+require_once 'footer.html';
+?>
